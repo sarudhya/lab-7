607e13756215466b794cf87d23af6fedb524b473
sarudhya <sarudhya.achavakul@gmail.com>
2024-12-03 16:30:14 +0700

    Lab 7 completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -16,3 +16,37 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+#include<iostream>
+using namespace std;
+
+int gear(int x){
+   return x = (x/10000000) - 12;
+}
+
+int main(){
+    string name,Movie,date,wish;
+    int x;
+    
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! " << name <<" is a really cool name.\n";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << name <<": ";
+    cin >> x;
+    cout << "Fahsai: I think you may be GEAR "<< gear(x) << ". I have a free movie ticket for you.\n";
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+    cout << "Fahsai: What movie do you want to watch?\n";
+    cout << name <<": ";
+    cin.ignore();
+    getline(cin,Movie);
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << name <<": ";
+    getline(cin,date);
+    cout << "Fahsai: " << date <<"....that is OK!!! I'm looking forward to watching "<< Movie <<" with you.\n";
+    cout << name <<": ";
+    getline(cin,wish);
+    cout << "Fahsai: 555+ see you "<< date << ". Bye Bye \\(^ ^)/\n";
+
+}
